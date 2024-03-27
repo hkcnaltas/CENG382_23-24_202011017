42ab5421f521c5d5835226d1e76f3978002a7368
hkcnaltas <altashikmetcan@gmail.com>
2024-03-27 11:43:52 +0300

    lab5 functional clear button added

---
+++ RazorPagesMovie/Pages/Lab5Page.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class nameOfThePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
